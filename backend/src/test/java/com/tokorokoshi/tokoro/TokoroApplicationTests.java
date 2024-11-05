e32e560b533895f460035e066a30be77cbe4aaad
Alex Ivanov <oleksii120@gmail.com>
2024-11-05 21:59:14 +0200

    Add backend part

---
+++ backend/src/test/java/com/tokorokoshi/tokoro/TokoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokorokoshi.tokoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
