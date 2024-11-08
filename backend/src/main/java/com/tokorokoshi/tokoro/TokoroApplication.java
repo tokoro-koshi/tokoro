a519a18eeca5d8312869f3ea7d6f53ab907994dc
Alex Ivanov <oleksii120@gmail.com>
2024-11-08 19:43:03 +0200

    Add application configurations & secrets handling

--- backend/src/main/java/com/tokorokoshi/tokoro/TokoroApplication.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/TokoroApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TokoroApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, Tokoro!");
         SpringApplication.run(TokoroApplication.class, args);
     }
 }
---
+++ backend/src/main/java/com/tokorokoshi/tokoro/configuration/ApplicationConfiguration.java
@@ -0,0 +1,13 @@
+package com.tokorokoshi.tokoro.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true),
+        @PropertySource(value = "classpath:secrets.properties", ignoreResourceNotFound = true)
+})
+public class ApplicationConfiguration {
+}
