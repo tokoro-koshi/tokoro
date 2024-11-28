f7cb4600ef8b4b58de7fcd3e3dfff2f3f55afa7f
Alex Ivanov <oleksii120@gmail.com>
2024-11-29 00:20:27 +0200

    Update gitignore

--- backend/src/main/java/com/tokorokoshi/tokoro/modules/ai/AiClientService.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/ai/AiClientService.java
@@ -7,11 +7,12 @@ public interface AiClientService {
 
     /**
      * Get a string response from AI model.
-     * @param prompt prompt to send to AI model
+     *
+     * @param prompt         prompt to send to AI model
      * @param conversationId conversation id
-     * @param model model to use (check language model documentation)
-     * @param maxTokens maximum tokens (check language model documentation)
-     * @param temperature temperature (check language model documentation)
+     * @param model          model to use (check language model documentation)
+     * @param maxTokens      maximum tokens (check language model documentation)
+     * @param temperature    temperature (check language model documentation)
      * @return response from AI model
      * @throws IllegalStateException if prompt is null
      */
@@ -25,13 +26,14 @@ public interface AiClientService {
 
     /**
      * Get a structured response from AI model.
-     * @param prompt prompt to send to AI model
-     * @param responseType response type
+     *
+     * @param prompt         prompt to send to AI model
+     * @param responseType   response type
      * @param conversationId conversation id
-     * @param model model to use (check language model documentation)
-     * @param maxTokens maximum tokens (check language model documentation)
-     * @param temperature temperature (check language model documentation)
-     * @param <T> structured response type
+     * @param model          model to use (check language model documentation)
+     * @param maxTokens      maximum tokens (check language model documentation)
+     * @param temperature    temperature (check language model documentation)
+     * @param <T>            structured response type
      * @return Response from AI model, which can be successful and contain a generated item,
      * or unsuccessful and contain a string refusal reason
      */
