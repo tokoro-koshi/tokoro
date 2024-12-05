a0437e497f999602b0387d5dc4b5d07aa43ffd96
AndrewVynnytskyi <146383822+AndrewVynnytskyi@users.noreply.github.com>
2024-12-05 22:36:07 +0200

    Add UserRatingController and UserRatingService

--- backend/src/main/java/com/tokorokoshi/tokoro/modules/userRating/UserRatingService.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/userRating/UserRatingService.java
@@ -3,6 +3,8 @@ package com.tokorokoshi.tokoro.modules.userRating;
 
 import com.tokorokoshi.tokoro.database.PromptHistory;
 import com.tokorokoshi.tokoro.database.UserRating;
+import com.tokorokoshi.tokoro.modules.userRating.dto.CreateUpdateUserRatingDto;
+import com.tokorokoshi.tokoro.modules.userRating.dto.UserRatingDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
@@ -11,33 +13,47 @@ import java.util.List;
 
 @Service
 public class UserRatingService {
-    MongoTemplate mongoTemplate;
+    private final MongoTemplate mongoTemplate;
+    private final UserRatingMapper userRatingMapper;
 
     @Autowired
-    public UserRatingService(MongoTemplate mongoTemplate){
+    public UserRatingService(MongoTemplate mongoTemplate, UserRatingMapper userRatingMapper) {
         this.mongoTemplate = mongoTemplate;
+        this.userRatingMapper = userRatingMapper;
     }
+
 
     //Crud
 
-    public void saveUserRating(UserRating userRating){
-        mongoTemplate.save(userRating);
+    public UserRatingDto saveUserRating(CreateUpdateUserRatingDto userRating){
+        return userRatingMapper.
+                toUserRatingDto(mongoTemplate.
+                        save(userRatingMapper.
+                                toUserRatingSchema(userRating)));
     }
 
-    public UserRating findUserRatingById(String id){
-        return mongoTemplate.findById(id, UserRating.class);
+    public UserRatingDto findUserRatingById(String id){
+        return userRatingMapper.
+                toUserRatingDto(mongoTemplate.
+                        findById(id, UserRating.class));
     }
 
-    public List<UserRating> findAllUserRatings(){
-        return mongoTemplate.findAll(UserRating.class);
+    public List<UserRatingDto> findAllUserRatings(){
+        return userRatingMapper.
+                toUserRatingDto(mongoTemplate.
+                        findAll(UserRating.class));
     }
 
-    public void updateUserRating(String id, UserRating userRating){
-        userRating.setId(id);
-        mongoTemplate.save(userRating);
+    public UserRatingDto updateUserRating(String id, CreateUpdateUserRatingDto userRating){
+        UserRating userRatingSchema = userRatingMapper.toUserRatingSchema(userRating);
+        userRatingSchema.setId(id);
+        return userRatingMapper.
+                toUserRatingDto(mongoTemplate.
+                        save(userRatingSchema));
+
     }
 
     public void deleteUserRating(String id){
-        mongoTemplate.remove(findUserRatingById(id));
+        mongoTemplate.remove(userRatingMapper.toUserRatingSchema(findUserRatingById(id)));
     }
 }
