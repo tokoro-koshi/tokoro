f9a88238734f45815609f6876215359fe7f2cba1
Alex Ivanov <oleksii120@gmail.com>
2024-12-09 08:42:53 +0200

    Convert schema classes into records (to avoid using Lombok library)

--- backend/src/main/java/com/tokorokoshi/tokoro/database/Place.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/database/Place.java
@@ -9,96 +9,23 @@ import org.springframework.data.mongodb.core.mapping.Field;
 import java.util.List;
 
 @Document(collection = "place")
-public class Place {
-    @Id
-    private String id;
-
-    @NonNull
-    @Indexed(unique = true)
-    private String name;
-
-    private String description;
-
-    @NonNull
-    @Field
-    private Location location;
-
-    private String categoryId;
-
-
-    @NonNull
-    private List<HashTag> hashTags;
-
-    @NonNull
-    private double rating;
-
-    public Place() {
-        this.name = "";
-        this.description = "";
-        this.location = new Location();
-        this.hashTags = List.of();
-        this.rating = 0;
-    }
-
-    @NonNull
-    public String getId() {
-        return id;
-    }
-
-    public void setId(@NonNull String id) {
-        this.id = id;
-    }
-
-    @NonNull
-    public String getName() {
-        return name;
-    }
-
-    public void setName(@NonNull String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @NonNull
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(@NonNull Location location) {
-        this.location = location;
-    }
-
-    @NonNull
-    public String getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(@NonNull String categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    @NonNull
-    public List<HashTag> getHashTags() {
-        return hashTags;
-    }
-
-    public void setHashTags(@NonNull List<HashTag> hashTags) {
-        this.hashTags = hashTags;
-    }
-
-    @NonNull
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(@NonNull double rating) {
-        this.rating = rating;
-    }
+public record Place(
+        @Id
+        String id,
+        @NonNull
+        @Indexed(unique = true)
+        String name,
+        String description,
+        @NonNull
+        @Field
+        Location location,
+        String categoryId,
+        @NonNull
+        List<HashTag> hashTags,
+        @NonNull
+        double rating
+) {
+        public Place withId(String id) {
+                return new Place(id, name, description, location, categoryId, hashTags, rating);
+        }
 }
--- backend/src/main/java/com/tokorokoshi/tokoro/database/UserFavorites.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/database/UserFavorites.java
@@ -1,36 +1,14 @@
 package com.tokorokoshi.tokoro.database;
 
 import com.mongodb.lang.NonNull;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-
-public class UserFavorites {
-    private List<String> places;
-    private List<String> prompts;
-
-    public UserFavorites() {
-        this.places = List.of();
-        this.prompts = List.of();
-    }
-
-    @NonNull
-    public List<String> getPlaces() {
-        return places;
-    }
-
-    public void setPlaces(@NonNull List<String> places) {
-        this.places = places;
-    }
-
-    @NonNull
-    public List<String> getPrompts() {
-        return prompts;
-    }
-
-    public void setPrompts(@NonNull List<String> prompts) {
-        this.prompts = prompts;
-    }
-}
+@Document
+public record UserFavorites(
+        @NonNull
+        List<String> places,
+        @NonNull
+        List<String> prompts
+) {}
