e2b686d61ffce02b6ed9b5c0542a8268f9e0f4cb
Alex Ivanov <oleksii120@gmail.com>
2024-12-09 08:27:20 +0200

    Convert DTO classes into records (to avoid using Lombok library)

--- backend/src/main/java/com/tokorokoshi/tokoro/modules/places/dto/CreateUpdatePlaceDto.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/places/dto/CreateUpdatePlaceDto.java
@@ -2,72 +2,11 @@ package com.tokorokoshi.tokoro.modules.places.dto;
 
 import java.util.List;
 
-public class CreateUpdatePlaceDto {
-
-    private String name;
-    private String description;
-    private LocationDto locationDto;
-    private String categoryId;
-    private List<HashTagDto> hashTagsDto;
-    private double rating;
-
-    public CreateUpdatePlaceDto() {
-        this.name = "";
-        this.description = "";
-        this.locationDto = new LocationDto();
-        this.categoryId = "";
-        this.hashTagsDto = List.of();
-        this.rating = 0;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocationDto getLocationDto() {
-        return locationDto;
-    }
-
-    public void setLocationDto(LocationDto location) {
-        this.locationDto = location;
-    }
-
-    public String getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public List<HashTagDto> getHashTagsDto() {
-        return hashTagsDto;
-    }
-
-    public void setHashTagsDto(List<HashTagDto> hashTags) {
-        this.hashTagsDto = hashTags;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-
-}
-
+public record CreateUpdatePlaceDto(
+        String name,
+        String description,
+        LocationDto locationDto,
+        String categoryId,
+        List<HashTagDto> hashTagsDto,
+        double rating
+) {}
--- backend/src/main/java/com/tokorokoshi/tokoro/modules/places/dto/HashTagDto.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/places/dto/HashTagDto.java
@@ -1,28 +1,6 @@
 package com.tokorokoshi.tokoro.modules.places.dto;
 
-public class HashTagDto {
-    private String id;
-    private String name;
-
-    public HashTagDto() {
-        this.id = "";
-        this.name = "";
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-}
+public record HashTagDto(
+        String id,
+        String name
+) {}
