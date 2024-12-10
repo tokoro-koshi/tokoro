81478035d9619b9f9c21191cd0d12670336884d2
Danylo Shliakhetko <danielshliakhetko@gmail.com>
2024-12-10 22:13:45 +0200

    Setup frontend dependencies (#10)
    Co-authored-by: Alex Ivanov <oleksii120@gmail.com>

---
+++ frontend/app/api/auth/[auth0]/route.ts
@@ -0,0 +1,4 @@
+﻿// import auth0 from "@/lib/helpers/auth0";
+import { handleAuth } from '@auth0/nextjs-auth0';
+
+export const GET = handleAuth();
---
+++ frontend/lib/helpers/auth0.ts
@@ -0,0 +1,14 @@
+﻿import { AccessTokenError, getAccessToken } from '@auth0/nextjs-auth0';
+
+export async function getBearerToken(): Promise<string | null> {
+  try {
+    const token = (await getAccessToken()).accessToken;
+    if (!token) return null;
+    return `Bearer ${token}`;
+  } catch (e) {
+    if (!(e instanceof AccessTokenError) || e.code !== 'ERR_MISSING_SESSION')
+      console.error(e);
+
+    return null;
+  }
+}
