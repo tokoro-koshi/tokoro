010852a7761468bb02c016a66f3e234f565f69af
daniel-shliakhetko <danielshliakhetko@gmail.com>
2024-12-10 22:01:41 +0200

    Setup frontend dependencies
    Co-authored-by: Alex Ivanov <oleksii120@gmail.com>

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,21 +1,23 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import '@/app/styles/globals.css';
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Tokoro",
-  description: "",
+  title: 'Tokoro',
+  description: '',
 };
 
 export default function RootLayout({
@@ -24,11 +26,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <UserProvider>{children}</UserProvider>
       </body>
     </html>
   );
---
+++ frontend/lib/helpers/apiClient.ts
@@ -0,0 +1,21 @@
+﻿import axios from 'axios';
+import { getBearerToken } from '@/lib/helpers/auth0';
+
+const headers = {
+  'Content-Type': 'application/json',
+};
+
+const apiClient = axios.create({
+  baseURL: process.env.API_URL,
+  headers: headers,
+});
+
+apiClient.interceptors.request.use(async (config) => {
+  const token = await getBearerToken();
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
+}, Promise.reject);
+
+export default apiClient;
