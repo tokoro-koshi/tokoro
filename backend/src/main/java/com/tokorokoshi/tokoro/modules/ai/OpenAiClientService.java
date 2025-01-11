601fc8d2b0e0a79f0ec05af92d10dfd0c5e84e7b
AndrewVynnytskyi <146383822+AndrewVynnytskyi@users.noreply.github.com>
2025-01-11 23:21:37 +0200

    Implemented getResponse method in Ai service without response schema
    Co-authored-by: Alex Ivanov <oleksii120@gmail.com>

--- backend/src/main/java/com/tokorokoshi/tokoro/modules/ai/OpenAiClientService.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/ai/OpenAiClientService.java
@@ -3,15 +3,19 @@ package com.tokorokoshi.tokoro.modules.ai;
 import com.tokorokoshi.tokoro.modules.json.JsonHelper;
 import jakarta.annotation.Nullable;
 import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.ai.chat.messages.*;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatOptions;
-import org.springframework.ai.openai.api.OpenAiApi.ChatCompletionRequest.ResponseFormat;
+import org.springframework.ai.openai.api.ResponseFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class OpenAiClientService implements AiClientService {
@@ -22,6 +26,16 @@ public class OpenAiClientService implements AiClientService {
         this.chatClient = chatClient;
     }
 
+    /**
+     * This builder will generate JSON options for OpenAI fetch query
+     * @param responseJsonSchema JSON of schema for GPT to adhere
+     *
+     * @param conversationId Conversation ID
+     * @param model OpenAI Text model
+     * @param maxTokens Maximum tokens for processing
+     * @param temperature Temperature (randomness, [0, 2])
+     * @return Options for OpenAI prompt
+     */
     private static OpenAiChatOptions getOptions(
             String responseJsonSchema,
             String conversationId,
@@ -29,7 +43,6 @@ public class OpenAiClientService implements AiClientService {
             Integer maxTokens,
             Double temperature
     ) {
-        // This builder will generate similar JSON options for OpenAI fetch query
         /*
         {
             "model": "gpt-4o-mini";
@@ -44,13 +57,13 @@ public class OpenAiClientService implements AiClientService {
 
         OpenAiChatOptions.Builder optionsBuilder = OpenAiChatOptions.builder();
         if (model != null) {
-            optionsBuilder.withModel(model);
+            optionsBuilder.model(model);
         }
         if (maxTokens != null) {
-            optionsBuilder.withMaxTokens(maxTokens);
+            optionsBuilder.maxTokens(maxTokens);
         }
         if (conversationId != null) {
-            optionsBuilder.withToolContext(
+            optionsBuilder.toolContext(
                     Map.of(
                             "chatMemory",
                             Map.of(
@@ -61,15 +74,51 @@ public class OpenAiClientService implements AiClientService {
             );
         }
         if (temperature != null) {
-            optionsBuilder.withTemperature(temperature);
+            optionsBuilder.temperature(temperature);
         }
         if (responseJsonSchema != null) {
-            optionsBuilder.withResponseFormat(new ResponseFormat(
+            optionsBuilder.responseFormat(new ResponseFormat(
                     ResponseFormat.Type.JSON_SCHEMA,
                     responseJsonSchema
             ));
         }
         return optionsBuilder.build();
+    }
+
+    /**
+     * This function maps custom messages iterable
+     * into OpenAI API compliant classes instances
+     *
+     * @param messages Messages in chat history
+     * @return Messages in chat history suitable for OpenAI API
+     */
+    private static List<org.springframework.ai.chat.messages.Message> mapMessages(
+            Iterable<Message> messages
+    ) {
+        var messagesList
+                = new ArrayList<org.springframework.ai.chat.messages.Message>();
+        for (Message message : messages) {
+            switch (message.role()) {
+                case USER -> messagesList.add(
+                        new UserMessage(
+                                message.content()
+                        )
+                );
+                case ASSISTANT -> messagesList.add(
+                        new AssistantMessage(
+                                message.content()
+                        )
+                );
+                case SYSTEM -> messagesList.add(
+                        new SystemMessage(
+                                message.content()
+                        )
+                );
+                case null, default ->
+                        throw new IllegalStateException("Message role was set incorrectly");
+            }
+        }
+        return messagesList;
     }
 
     @Override
@@ -80,31 +129,16 @@ public class OpenAiClientService implements AiClientService {
             Integer maxTokens,
             Double temperature
     ) {
-        if (strPrompt == null) {
-            throw new IllegalArgumentException("Message cannot be null");
-        }
-
-        Prompt prompt = new Prompt(
-                strPrompt,
-                OpenAiClientService.getOptions(
-                        null,
-                        conversationId,
-                        model,
-                        maxTokens,
-                        temperature
-                )
+        return this.getResponse(
+                List.of(new Message(
+                        MessageType.USER,
+                        Objects.requireNonNull(strPrompt)
+                )),
+                conversationId,
+                model,
+                maxTokens,
+                temperature
         );
-
-        try {
-            return this.chatClient
-                    .call(prompt)
-                    .getResult()
-                    .getOutput()
-                    .getContent();
-        } catch (Exception e) {
-            String[] components = e.getMessage().split(" - ");
-            return components.length > 1 ? components[1] : components[0];
-        }
     }
 
     @Override
@@ -116,17 +150,12 @@ public class OpenAiClientService implements AiClientService {
             Integer maxTokens,
             Double temperature
     ) {
-        if (strPrompt == null) {
-            throw new IllegalArgumentException("Message cannot be null");
-        }
-        if (responseType == null) {
-            throw new IllegalArgumentException("Response type cannot be null");
-        }
-
         Prompt prompt = new Prompt(
-                strPrompt,
+                Objects.requireNonNull(strPrompt),
                 OpenAiClientService.getOptions(
-                        JsonHelper.getJsonSchema(responseType),
+                        JsonHelper.getJsonSchema(
+                                Objects.requireNonNull(responseType)
+                        ),
                         conversationId,
                         model,
                         maxTokens,
@@ -168,21 +197,39 @@ public class OpenAiClientService implements AiClientService {
 
     @Override
     public String getResponse(
-            String prompt,
-            Message[] messages,
+            List<Message> messages,
             @Nullable String conversationId,
             @Nullable String model,
             @Nullable Integer maxTokens,
             @Nullable Double temperature
     ) {
-        // TODO
-        throw new NotImplementedException();
+
+        Prompt chatPrompt = new Prompt(
+                OpenAiClientService.mapMessages(Objects.requireNonNull(messages)),
+                OpenAiClientService.getOptions(
+                        null,
+                        conversationId,
+                        model,
+                        maxTokens,
+                        temperature
+                )
+        );
+
+        try {
+            return this.chatClient
+                    .call(chatPrompt)
+                    .getResult()
+                    .getOutput()
+                    .getContent();
+        } catch (Exception e) {
+            String[] components = e.getMessage().split(" - ");
+            return components.length > 1 ? components[1] : components[0];
+        }
     }
 
     @Override
     public <T> Response<T> getResponse(
-            String prompt,
-            Message[] messages,
+            List<Message> messages,
             Class<T> responseType,
             @Nullable String conversationId,
             @Nullable String model,
