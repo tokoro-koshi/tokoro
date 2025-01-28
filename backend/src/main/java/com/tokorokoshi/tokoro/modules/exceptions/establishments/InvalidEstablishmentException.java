185c79b5e3d9faa251cbad172eade6860244f882
Oleh Popovych <oleh.popovych.shi.2023@lpnu.ua>
2025-01-28 21:26:44 +0200

    Add custom exceptions for establishments

---
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/exceptions/establishments/InvalidEstablishmentException.java
@@ -0,0 +1,7 @@
+package com.tokorokoshi.tokoro.modules.exceptions.establishments;
+
+public class InvalidEstablishmentException extends IllegalArgumentException {
+    public InvalidEstablishmentException(String message) {
+        super(message);
+    }
+}
