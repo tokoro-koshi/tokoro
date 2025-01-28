7735ad335e4a455871a3bb0d6d845c6cc7df8a6c
Oleh Popovych <oleh.popovych.shi.2023@lpnu.ua>
2025-01-28 21:24:12 +0200

    Hotfix Comment database entry

--- backend/src/main/java/com/tokorokoshi/tokoro/database/Comment.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/database/Comment.java
@@ -1,6 +1,6 @@
 package com.tokorokoshi.tokoro.database;
 
-import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
