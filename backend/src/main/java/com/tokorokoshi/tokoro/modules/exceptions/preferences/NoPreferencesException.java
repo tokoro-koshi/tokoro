0ed95b902b3698839807b5c79182ac5b15501c96
Oleh Popovych <oleh.popovych.shi.2023@lpnu.ua>
2025-01-28 21:27:48 +0200

    Add custom exceptions for preferences

---
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/exceptions/preferences/NoPreferencesException.java
@@ -0,0 +1,7 @@
+package com.tokorokoshi.tokoro.modules.exceptions.preferences;
+
+public class NoPreferencesException extends RuntimeException {
+    public NoPreferencesException(String message) {
+        super(message);
+    }
+}
