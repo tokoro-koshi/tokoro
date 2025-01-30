0e25bf3e0d43861070c2d2fdac1cbc65b4fdd5cb
Alex Ivanov <oleksii120@gmail.com>
2025-01-30 13:56:38 +0200

    Add file service implementation

---
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/file/S3Constants.java
@@ -0,0 +1,8 @@
+package com.tokorokoshi.tokoro.modules.file;
+
+public class S3Constants {
+    public static final int COMMON_EXPIRATION = 5 * 60;
+    public static final String ROOT_FOLDER = "uploads/";
+    public static final String USER_FOLDER = "users/";
+    public static final String DIRECTORY_TYPE = "directory";
+}
