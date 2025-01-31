2b9f964de28188d24ac3d89c9e39e93f1eb077c9
Ania Savchuk <Ania.Savchuk.ShI.2023@lpnu.ua>
2025-01-31 20:29:59 +0200

    Auth buttons refactor

---
+++ frontend/components/buttons/register/Register.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import routes from "@/lib/constants/routes";
+import styles from "@/components/buttons/register/register.module.css";
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+
+export default function RegisterButton() {
+
+    return (
+        <Link
+            href={routes.auth.register}
+            className={cn(
+                styles.registerButton,
+                buttonVariants({ variant: "outline" })
+            )}
+        >
+            Register
+        </Link>
+    )
+}
+
