fa3994e2135473c597a4751756110d7ed0227b94
Alex Ivanov <oleksii120@gmail.com>
2025-02-01 15:04:22 +0200

    Add user state management

--- frontend/components/buttons/AuthButtons.tsx
+++ frontend/components/buttons/AuthButtons.tsx
@@ -5,7 +5,11 @@ import { Button } from '@/components/buttons/button';
 import routes from '@/lib/constants/routes';
 import styles from './auth-buttons.module.css';
 
-export default function AuthButton({ user }: { user?: UserProfile }) {
+type AuthButtonProps = {
+  user?: UserProfile | null;
+};
+
+export default function AuthButton({ user }: AuthButtonProps) {
   return (
     <Button
       className={styles.roundedBtn}
