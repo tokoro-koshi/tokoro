e4b36554f48e21b25c5a4fb94da0733bec63fe8b
Ania Savchuk <Ania.Savchuk.ShI.2023@lpnu.ua>
2025-02-01 13:42:41 +0200

    Update footer

--- frontend/lib/constants/routes.ts
+++ frontend/lib/constants/routes.ts
@@ -1,11 +1,17 @@
 ﻿const routes = {
   home: '/',
+  about: '/about',
+  privacyPolicy: '/privacy',
+  termsOfService: '/tos',
+  contactUs: '/contact',
+
   auth: {
     login: '/api/auth/login',
     logout: '/api/auth/logout',
     register: '/api/auth/signup',
     me: '/api/auth/me',
   },
+
   api: {
     
   },
