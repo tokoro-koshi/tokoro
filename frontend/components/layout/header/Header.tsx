106772d653267fc32c32c304cb794825c153a43e
Ania Savchuk <Ania.Savchuk.ShI.2023@lpnu.ua>
2025-02-01 13:41:10 +0200

    Update header and buttons

--- frontend/components/layout/header/Header.tsx
+++ frontend/components/layout/header/Header.tsx
@@ -2,13 +2,14 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
+import {useUser} from "@auth0/nextjs-auth0/client";
 import routes from '@/lib/constants/routes';
-import styles from './Header.module.css'
 import Logo from '@/public/logo_without_text.svg'
-import AuthButtons from '@/components/buttons/AuthButtons'
-import React from "react";
+import AuthButton from '@/components/buttons/AuthButtons'
+import styles from './Header.module.css'
 
 export default function Header() {
+    const { user, isLoading } = useUser();
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
@@ -22,12 +23,16 @@ export default function Header() {
                 <Link href='#features' className={styles.navLink}>
                     Features
                 </Link>
-                <Link href={'/about'} className={styles.navLink}>
+                <Link href={routes.about} className={styles.navLink}>
                     About
                 </Link>
+                {!user && !isLoading && (
+                    <Link href={routes.auth.login} className={styles.navLink}>
+                        Login
+                    </Link>
+                )}
             </nav>
-            <AuthButtons />
-
+            <AuthButton user={user} />
         </header>
     );
 }
