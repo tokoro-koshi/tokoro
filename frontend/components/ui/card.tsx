e6830d6ca221689c41cb419b817e1504675f6b0c
Ania Savchuk <Ania.Savchuk.ShI.2023@lpnu.ua>
2025-02-02 15:18:01 +0200

    Update design how-it-works section and deleted unused section

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'rounded-lg border bg-card text-card-foreground shadow-sm',
+      'rounded-[43px] bg-card text-center w-full shadow-xl',
       className
     )}
     {...props}
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn('flex flex-col space-y-1.5 p-6', className)}
+    className={cn('flex flex-col space-y-0 py-9', className)}
     {...props}
   />
 ));
@@ -36,7 +36,7 @@ const CardTitle = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'text-2xl font-semibold leading-none tracking-tight',
+      'text-3xl font-extrabold leading-none tracking-tight',
       className
     )}
     {...props}
@@ -50,7 +50,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn('text-sm text-muted-foreground', className)}
+    className={cn('text-xl text-primary' , className)}
     {...props}
   />
 ));
