452f8561bfb99d522d6102b388c2b94579517e91
Alex Ivanov <oleksii120@gmail.com>
2025-02-02 17:10:36 +0200

    Fix create/update place DTO

--- backend/src/main/java/com/tokorokoshi/tokoro/modules/places/dto/CreateUpdatePlaceDto.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/places/dto/CreateUpdatePlaceDto.java
@@ -1,13 +1,10 @@
 package com.tokorokoshi.tokoro.modules.places.dto;
 
-import java.util.List;
-
 public record CreateUpdatePlaceDto(
     String name,
     String description,
     LocationDto location,
     String categoryId,
-    List<String> pictures,
     double rating
 ) {
 }
