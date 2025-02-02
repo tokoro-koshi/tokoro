91d6bba5fc48e83b79b1c5f573c7a99a41daf9b5
Alex Ivanov <oleksii120@gmail.com>
2025-02-02 16:20:34 +0200

    Improve DTO mappings

--- backend/src/main/java/com/tokorokoshi/tokoro/modules/places/dto/CreateUpdatePlaceDto.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/places/dto/CreateUpdatePlaceDto.java
@@ -1,13 +1,10 @@
 package com.tokorokoshi.tokoro.modules.places.dto;
 
-import java.util.List;
-
 public record CreateUpdatePlaceDto(
     String name,
     String description,
-    LocationDto locationDto,
+    LocationDto location,
     String categoryId,
-    List<HashTagDto> hashTagsDto,
     double rating
 ) {
 }
