3e16d4bb99b6a4722e5eafb35d298c0412a03c13
Alex Ivanov <oleksii120@gmail.com>
2025-02-03 17:24:44 +0200

    Fix tag handling

--- backend/src/main/java/com/tokorokoshi/tokoro/database/HashTag.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/database/HashTag.java
@@ -1,13 +1,8 @@
 package com.tokorokoshi.tokoro.database;
 
 import com.mongodb.lang.NonNull;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "hashTag")
 public record HashTag(
-    @Id
-    String id,
     @NonNull
     String lang,
     @NonNull
