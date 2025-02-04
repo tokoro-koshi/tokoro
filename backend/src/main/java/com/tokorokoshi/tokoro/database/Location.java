84bf68e5a7d2e566edb1a8ebbf57e0e13206a774
Alex Ivanov <oleksii120@gmail.com>
2025-02-04 19:22:31 +0200

    Add docs for database entities

--- backend/src/main/java/com/tokorokoshi/tokoro/database/Location.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/database/Location.java
@@ -4,16 +4,19 @@ import com.mongodb.lang.NonNull;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+/**
+ * Represents a location in the database.
+ */
 @Document
 public record Location(
-    @NonNull
-    String address,
-    @NonNull
-    String city,
-    @NonNull
-    String country,
-    @NonNull
-    @Field
-    Coordinate coordinate
+        @NonNull
+        String address,
+        @NonNull
+        String city,
+        @NonNull
+        String country,
+        @NonNull
+        @Field
+        Coordinate coordinate
 ) {
 }
--- backend/src/main/java/com/tokorokoshi/tokoro/database/Place.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/database/Place.java
@@ -8,48 +8,63 @@ import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
+/**
+ * Represents a place in the database.
+ */
 @Document(collection = "place")
 public record Place(
-    @Id
-    String id,
-    @NonNull
-    @Indexed(unique = true)
-    String name,
-    String description,
-    @NonNull
-    @Field
-    Location location,
-    String categoryId,
-    @NonNull
-    List<HashTag> tags,
-    @NonNull
-    List<String> pictures,
-    @NonNull
-    double rating
+        @Id
+        String id,
+        @NonNull
+        @Indexed(unique = true)
+        String name,
+        String description,
+        @NonNull
+        @Field
+        Location location,
+        String categoryId,
+        @NonNull
+        List<HashTag> tags,
+        @NonNull
+        List<String> pictures,
+        @NonNull
+        double rating
 ) {
+    /**
+     * Creates a new place with the given pictures.
+     *
+     * @param pictures The list of pictures of the place
+     * @return A new place with the given pictures
+     */
     public Place withPictures(List<String> pictures) {
         return new Place(
-            id,
-            name,
-            description,
-            location,
-            categoryId,
-            tags,
-            pictures,
-            rating
+                id,
+                name,
+                description,
+                location,
+                categoryId,
+                tags,
+                pictures,
+                rating
         );
     }
 
+    /**
+     * Creates a new place with the given tags.
+     *
+     * @param tags The list of tags of the place
+     * @return A new place with the given tags
+     */
     public Place withTags(List<HashTag> tags) {
         return new Place(
-            id,
-            name,
-            description,
-            location,
-            categoryId,
-            tags,
-            pictures,
-            rating
+                id,
+                name,
+                description,
+                location,
+                categoryId,
+                tags,
+                pictures,
+                rating
         );
     }
 }
