038bc1a8c2d016f9a8828ecf3d361de00520187c
Alex Ivanov <oleksii120@gmail.com>
2025-02-04 16:45:07 +0200

    Add pagination for places

--- backend/src/main/java/com/tokorokoshi/tokoro/modules/places/PlacesController.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/places/PlacesController.java
@@ -2,6 +2,9 @@ package com.tokorokoshi.tokoro.modules.places;
 
 import com.tokorokoshi.tokoro.modules.places.dto.CreateUpdatePlaceDto;
 import com.tokorokoshi.tokoro.modules.places.dto.PlaceDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,10 +23,10 @@ public class PlacesController {
     }
 
     @PostMapping(value = {"", "/"},
-        consumes = MULTIPART_FORM_DATA_VALUE,
-        produces = APPLICATION_JSON_VALUE)
+            consumes = MULTIPART_FORM_DATA_VALUE,
+            produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<PlaceDto> savePlace(
-        @ModelAttribute CreateUpdatePlaceDto place
+            @ModelAttribute CreateUpdatePlaceDto place
     ) {
         return ResponseEntity.ok(placesService.savePlace(place));
     }
@@ -34,23 +37,27 @@ public class PlacesController {
     }
 
     @GetMapping(value = {"", "/"}, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<PlaceDto>> getAllPlaces() {
-        return ResponseEntity.ok(placesService.getAllPlaces());
+    public ResponseEntity<Page<PlaceDto>> getAllPlaces(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(placesService.getAllPlaces(pageable));
     }
 
     @GetMapping(value = "/random/{count}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<PlaceDto>> getRandomPlaces(
-        @PathVariable int count
+            @PathVariable int count
     ) {
         return ResponseEntity.ok(placesService.getRandomPlaces(count));
     }
 
     @PutMapping(value = "/{id}",
-        consumes = MULTIPART_FORM_DATA_VALUE,
-        produces = APPLICATION_JSON_VALUE)
+            consumes = MULTIPART_FORM_DATA_VALUE,
+            produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<PlaceDto> updatePlace(
-        @PathVariable String id,
-        @ModelAttribute CreateUpdatePlaceDto place
+            @PathVariable String id,
+            @ModelAttribute CreateUpdatePlaceDto place
     ) {
         PlaceDto updatedPlace = placesService.updatePlace(id, place);
         if (updatedPlace == null) {
