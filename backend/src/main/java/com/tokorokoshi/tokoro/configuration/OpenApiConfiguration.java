01465966ced66e6d0f56aa27c5b3cd2eec833cf4
Alex Ivanov <oleksii120@gmail.com>
2025-02-04 16:51:48 +0200

    Configure swagger for project

---
+++ backend/src/main/java/com/tokorokoshi/tokoro/configuration/OpenApiConfiguration.java
@@ -0,0 +1,16 @@
+package com.tokorokoshi.tokoro.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Tokoro API",
+                version = "1.0",
+                description = "This API provides the functionality to fullfill the Tokoro application needs in data querying and manipulation."
+        )
+)
+public class OpenApiConfiguration {
+}
