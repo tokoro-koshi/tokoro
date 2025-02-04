064d2c714be09bac523ddc35dfd861bc259ab908
Alex Ivanov <oleksii120@gmail.com>
2025-02-04 19:07:55 +0200

    Add docs for internal classes

--- backend/src/main/java/com/tokorokoshi/tokoro/modules/file/S3Constants.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/file/S3Constants.java
@@ -1,8 +1,23 @@
 package com.tokorokoshi.tokoro.modules.file;
 
+/**
+ * Constants for the S3 file storage module.
+ */
 public class S3Constants {
+    /**
+     * The expiration time for the signed URL.
+     */
     public static final int COMMON_EXPIRATION = 5 * 60;
+    /**
+     * The root folder in the S3 bucket.
+     */
     public static final String ROOT_FOLDER = "";
+    /**
+     * The folder for user files.
+     */
     public static final String USER_FOLDER = "users/";
+    /**
+     * The folder for blog files.
+     */
     public static final String DIRECTORY_TYPE = "directory";
 }
