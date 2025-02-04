8e49dbc0d53099d7c3371a94e276e67cda4df33d
Alex Ivanov <oleksii120@gmail.com>
2025-02-04 14:56:48 +0200

    Improve place DTOs

--- backend/src/main/java/com/tokorokoshi/tokoro/modules/places/PlaceMapper.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/places/PlaceMapper.java
@@ -1,16 +1,12 @@
 package com.tokorokoshi.tokoro.modules.places;
 
-import com.tokorokoshi.tokoro.database.HashTag;
 import com.tokorokoshi.tokoro.database.Place;
 import com.tokorokoshi.tokoro.modules.places.dto.CreateUpdatePlaceDto;
 import com.tokorokoshi.tokoro.modules.places.dto.PlaceDto;
-import com.tokorokoshi.tokoro.modules.tags.dto.TagDto;
-import com.tokorokoshi.tokoro.modules.tags.dto.TagsDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 @Mapper(componentModel = "spring")
 public interface PlaceMapper {
@@ -20,19 +16,4 @@ public interface PlaceMapper {
     PlaceDto toPlaceDto(Place place);
 
     List<PlaceDto> toPlaceDto(List<Place> places);
-
-    default TagsDto map(List<HashTag> tags) {
-        return new TagsDto(
-            tags.stream()
-                .map(tag -> new TagDto(tag.lang(), tag.name()))
-                .toArray(TagDto[]::new)
-        );
-    }
-
-    default List<HashTag> map(TagsDto tagsDto) {
-        return Stream.of(tagsDto.tags())
-                     .map(tagDto ->
-                              new HashTag(tagDto.lang(), tagDto.name())
-                     ).toList();
-    }
 }
