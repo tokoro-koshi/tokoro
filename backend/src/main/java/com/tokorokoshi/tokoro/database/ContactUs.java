4507bf448f19f565463c3c32f6ee62b92485104b
Oleh Popovych <oleh.popovych.shi.2023@lpnu.ua>
2025-02-09 14:41:48 +0200

    Update ContactUs entity by adding proper docs

--- backend/src/main/java/com/tokorokoshi/tokoro/database/ContactUs.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/database/ContactUs.java
@@ -1,11 +1,16 @@
 package com.tokorokoshi.tokoro.database;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+/**
+ * Represents a 'Contact us' section in the database.
+ */
 @Document
 public record ContactUs(
+        @NotNull
         List<String> socialLinks,
         String phoneNumber
 ) {
