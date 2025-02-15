3e135f24107ab20137cdbdd734e9fc80b4e0c96b
Alex Ivanov <oleksii120@gmail.com>
2025-02-16 00:31:20 +0200

    Enhance Swagger configuration to include security scheme for JWT authentication

--- backend/src/main/java/com/tokorokoshi/tokoro/configuration/SwaggerConfiguration.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/configuration/SwaggerConfiguration.java
@@ -2,13 +2,45 @@ package com.tokorokoshi.tokoro.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition(
         info = @Info(
                 title = "Tokoro API",
-                version = "1.0",
+                version = "v1",
                 description = "This API provides the functionality to fullfill the Tokoro application needs in data querying and manipulation."
-        )
+        ),
+        security = {
+                @io.swagger.v3.oas.annotations.security.SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
 )
 public class SwaggerConfiguration {
+    private static final String securitySchemeName = "bearerAuth";
+    private static final String description = "Enter the JWT token with the `Bearer ` prefix, e.g. \"Bearer eyJhbGci...\"";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(
+                        new SecurityRequirement().addList(securitySchemeName)
+                )
+                .components(new Components().addSecuritySchemes(
+                                securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("Bearer")
+                                        .bearerFormat("JWT")
+                                        .description(description)
+                        )
+                );
+    }
 }
