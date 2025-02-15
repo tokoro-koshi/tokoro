97906a1bc871d527220f720375adfbb19318c9ae
Alex Ivanov <oleksii120@gmail.com>
2025-02-15 19:36:45 +0200

    Refactor image imports and update styles for consistency

--- frontend/fonts/index.ts
+++ frontend/fonts/index.ts
@@ -1,5 +1,7 @@
-import { Raleway as RalewayConstructor } from 'next/font/google';
-import { JetBrains_Mono as JetBrainsMonoConstructor } from 'next/font/google';
+import {
+  Raleway as RalewayConstructor,
+  JetBrains_Mono as JetBrainsMonoConstructor,
+} from 'next/font/google';
 
 export const Raleway = RalewayConstructor({
   subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext'],
@@ -14,3 +16,6 @@ export const JetBrainsMono = JetBrainsMonoConstructor({
   style: ['normal', 'italic'],
   variable: '--font-jetbrains-mono',
 });
+
+export const AllFonts = [Raleway, JetBrainsMono];
+export const AllFontsVariables = AllFonts.map((f) => f.variable).join(' ');
