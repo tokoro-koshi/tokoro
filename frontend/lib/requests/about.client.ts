202e389622c26a1d69ba4c99b9709e957841369d
daniel-shliakhetko <danielshliakhetko@gmail.com>
2025-02-17 21:55:18 +0200

    Add base request clients

---
+++ frontend/lib/requests/about.client.ts
@@ -0,0 +1,23 @@
+﻿import apiClient from '@/lib/helpers/apiClient';
+import { About } from '@/lib/types/about';
+
+export class AboutClient {
+  static async getAbout(): Promise<About> {
+    const response = await apiClient.get<About>(`/about`);
+    return response.data;
+  }
+
+  static async createAbout(about: About): Promise<About> {
+    const response = await apiClient.post<About>(`/about`, about);
+    return response.data;
+  }
+
+  static async updateAbout(about: About): Promise<About> {
+    const response = await apiClient.put<About>(`/about`, about);
+    return response.data;
+  }
+
+  static async deleteAbout(): Promise<void> {
+    await apiClient.delete(`/about`);
+  }
+}
