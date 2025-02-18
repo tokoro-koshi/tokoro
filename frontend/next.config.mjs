a47933d261b1487a4cb105f1777d3aba9874891c
daniel-shliakhetko <danielshliakhetko@gmail.com>
2025-02-18 21:02:50 +0200

    Add places to landing carousel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,13 @@
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tokoro.a4c55fc25483af7392a0ea7c5c114bfc.r2.cloudflarestorage.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
