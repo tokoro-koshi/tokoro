6fb6a090097ed43af65b30e2c943c326f3d99587
Oleh Popovych <oleh.popovych.shi.2023@lpnu.ua>
2025-02-22 15:18:47 +0200

    Add CreateUpdateReviewDto

---
+++ backend/src/main/java/com/tokorokoshi/tokoro/modules/reviews/dto/CreateUpdateReviewDto.java
@@ -0,0 +1,29 @@
+package com.tokorokoshi.tokoro.modules.reviews.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * A DTO for creating or updating a review
+ */
+@Schema(
+        name = "CreateUpdateReviewDto",
+        description = "A DTO for creating or updating a review"
+)
+public record CreateUpdateReviewDto(
+        @Schema(
+                name = "userId",
+                description = "The ID of the user who wrote the review"
+        )
+        String userId,
+        @Schema(
+                name = "placeId",
+                description = "The ID of the place being reviewed"
+        )
+        String placeId,
+        @Schema(
+                name = "comment",
+                description = "The comment of the review"
+        )
+        String comment
+) {
+}
