d8e953d8b268869b006113d3c6524a5891e02956
Alex Ivanov <oleksii120@gmail.com>
2025-03-06 18:13:21 +0200

    Add proper data fetching for explore page

--- frontend/app/explore/page.tsx
+++ frontend/app/explore/page.tsx
@@ -1,6 +1,8 @@
 import { PlaceClient } from '@/lib/requests/place.client';
 import Randomizer from '@/components/fyp/randomizer';
 import Sections from '@/components/fyp/sections';
+import { Place } from '@/lib/types/place';
+import { FavoritesClient } from '@/lib/requests/favorites.client';
 
 const sections = ['nearby', 'recommended', 'saved'] as const;
 
@@ -15,14 +17,29 @@ export default async function ForYouPage({ searchParams }: ForYouProps) {
     sections.find((x) => searchParams['section']?.trim().toLowerCase() === x) ??
     sections[0];
 
-  // TODO: Implement fetching data for each section
-  const random = await PlaceClient.getRandomPlaces(15);
+  let places: Place[];
+  switch (activeSection) {
+    case 'nearby': // TODO
+    case 'recommended':
+      places = await PlaceClient.getRandomPlaces(15);
+      break;
+    case 'saved':
+      const { payload: collections }
+        = await FavoritesClient.getAllCurrentUserCollections();
+      const placesIds = collections.reduce(
+        (acc, collection) => acc.concat(collection.placesIds),
+        [] as string[],
+      )
+
+      places = await PlaceClient.getPlacesByIdArray(placesIds);
+      break;
+  }
 
   return (
     <section className={'flex flex-col gap-6 pb-14'}>
       <Randomizer />
       <div className={'mx-9 h-2 flex-grow rounded-full bg-white'} />
-      <Sections activeSection={activeSection} places={random} />
+      <Sections activeSection={activeSection} places={places} />
     </section>
   );
 }
