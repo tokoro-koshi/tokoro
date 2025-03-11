32b8354260b68199c22d703e3d8f1025ee9a7282
Alex Ivanov <oleksii120@gmail.com>
2025-03-11 17:42:21 +0200

    Remove unused components

--- frontend/components/buttons/index.ts
+++ frontend/components/buttons/index.ts
@@ -1,6 +1,2 @@
 export * from './button';
-export * from './auth-buttons';
 export * from './explore/explore-buttons';
-export * from './login/login';
-export * from './logout/logout';
-export * from './register/register';
