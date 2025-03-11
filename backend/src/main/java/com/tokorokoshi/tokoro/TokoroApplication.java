e684f43361f80a2b0d163cdd209a4409053133f9
Alex Ivanov <oleksii120@gmail.com>
2025-03-11 17:18:09 +0200

    Speed up backend

--- backend/src/main/java/com/tokorokoshi/tokoro/TokoroApplication.java
+++ backend/src/main/java/com/tokorokoshi/tokoro/TokoroApplication.java
@@ -1,13 +1,23 @@
 package com.tokorokoshi.tokoro;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * The main class for the Tokoro application.
  * Here resides the main method that starts the Spring Boot application.
  */
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                MongoReactiveDataAutoConfiguration.class,
+                MetricsAutoConfiguration.class,
+                DataSourceAutoConfiguration.class,
+        },
+        scanBasePackages = "com.tokorokoshi.tokoro"
+)
 public class TokoroApplication {
     /**
      * Entry point for the Tokoro application.
