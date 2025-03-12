070437a0db6fab35c8ba8dea2dae594ac5e45427
Alex Ivanov <oleksii120@gmail.com>
2025-03-12 08:30:37 +0200

    Format files

--- frontend/components/ui/skeleton.tsx
+++ frontend/components/ui/skeleton.tsx
@@ -1,10 +1,7 @@
 import { HTMLAttributes } from 'react';
 import { cn } from '@/lib/utils';
 
-function Skeleton({
-  className,
-  ...props
-}: HTMLAttributes<HTMLDivElement>) {
+function Skeleton({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cn('animate-pulse rounded-md bg-muted', className)}
