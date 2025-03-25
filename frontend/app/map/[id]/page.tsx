4223214570caf1f0067ab73834914c5c1433e67b
Alex Ivanov <oleksii120@gmail.com>
2025-03-25 23:39:17 +0200

    Remove caching from map page

--- frontend/app/map/[id]/page.tsx
+++ frontend/app/map/[id]/page.tsx
@@ -2,14 +2,26 @@
 import { PlaceClient } from '@/lib/requests/place.client';
 import styles from '../map.module.css';
 
-export default async function PlaceMapPage({params}: {params: {id: string}}) {
+export default async function PlaceMapPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const place = await PlaceClient.getPlaceById(params.id);
-  const center = { lat: place.location.coordinate.latitude, lng: place.location.coordinate.longitude };
+  const center = {
+    lat: place.location.coordinate.latitude,
+    lng: place.location.coordinate.longitude,
+  };
 
   return (
     <div className={styles.pageContainer}>
       <div className={styles.mapWrapper}>
-        <GoogleMapComponent places={[place]} center={center} zoom={16} doesShowDetails={true} />
+        <GoogleMapComponent
+          places={[place]}
+          center={center}
+          zoom={16}
+          doesShowDetails={true}
+        />
       </div>
     </div>
   );
