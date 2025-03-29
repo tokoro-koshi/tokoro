1009ce1e862c5b4c9d218d63f2b355ce1892cede
Ania Savchuk <Ania.Savchuk.ShI.2023@lpnu.ua>
2025-03-29 20:27:39 +0200

    Add blog section

---
+++ frontend/lib/constants/blog/posts.ts
@@ -0,0 +1,52 @@
+export const blogPosts = [
+    {
+        id: 1,
+        imageUrl: '/placeholder.svg',
+        title: 'Denote simple fat denied add worthy little use!',
+        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,',
+        date: 'March 29, 2024'
+    },
+    {
+        id: 2,
+        imageUrl: '/placeholder.svg',
+        title: 'Exploring Hidden Gems of the City',
+        description: 'Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+        date: 'March 30, 2024'
+    },
+    {
+        id: 3,
+        imageUrl: '/placeholder.svg',
+        title: 'The Best Cafes in Town',
+        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+        date: 'March 31, 2024'
+    },
+
+    {
+        id: 4,
+        imageUrl: '/placeholder.svg',
+        title: 'The Ultimate Guide to Shopping',
+        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+        date: 'April 1, 2024'
+    },
+    {
+        id: 5,
+        imageUrl: '/placeholder.svg',
+        title: 'A Day in the Life of a Local',
+        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+        date: 'April 2, 2024'
+    },
+    {
+        id: 6,
+        imageUrl: '/placeholder.svg',
+        title: 'The Best Parks in the City',
+        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+        date: 'April 3, 2024'
+    },
+    {
+        id: 7,
+        imageUrl: '/placeholder.svg',
+        title: 'The Best Nightlife Spots',
+        description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+        date: 'April 4, 2024'
+    },
+];
