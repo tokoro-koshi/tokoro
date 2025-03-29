322e17b1b52fa890cc01f0316b3d092f7c9ef3ba
Ania Savchuk <Ania.Savchuk.ShI.2023@lpnu.ua>
2025-03-30 01:59:04 +0200

    Added loading of places and navigation through posts and place pages

--- frontend/components/cards/blog-card/blog-card.tsx
+++ frontend/components/cards/blog-card/blog-card.tsx
@@ -1,13 +1,14 @@
 import Image, { StaticImageData } from 'next/image';
-import { HTMLAttributes } from 'react';
 import styles from './blog-card.module.css';
 
-interface BlogCardProps extends HTMLAttributes<HTMLDivElement> {
+interface BlogCardProps {
     imageUrl: StaticImageData | string;
     title: string;
     description: string;
     date: string;
     imagePosition?: 'left' | 'right';
+    className?: string;
+    onClick?: () => void;
 }
 
 export const BlogCard = ({
@@ -17,26 +18,30 @@ export const BlogCard = ({
                              date,
                              imagePosition = 'left',
                              className = '',
+                             onClick,
                              ...props
                          }: BlogCardProps) => {
     return (
         <article
-            className={`flex flex-col  ${
-                imagePosition === 'left' ? 'md:flex-row' : 'md:flex-row-reverse '
+            className={`flex flex-col ${
+                imagePosition === 'left' ? 'md:flex-row' : 'md:flex-row-reverse'
             } gap-6 md:gap-8 ${className}`}
             {...props}
+            onClick={onClick}
+            role="button"
+            tabIndex={0}
         >
             <div className={`flex-shrink-0 ${
                 imagePosition === 'left'
                     ? 'md:-ml-16'
                     : 'md:-mr-16'
-            } md:-mb-8 `}>
+            } md:-mb-8`}>
                 <Image
                     src={imageUrl}
                     alt={title}
                     width={219}
                     height={219}
-                    className="rounded-[43px]"
+                    className="rounded-[43px] cursor-pointer"
                 />
             </div>
 
