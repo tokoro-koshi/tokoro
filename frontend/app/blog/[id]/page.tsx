50da55251230e6ad67519ee4dd451d3fca767f38
Ania Savchuk <Ania.Savchuk.ShI.2023@lpnu.ua>
2025-03-30 01:59:04 +0200

    Added loading of places and navigation through posts and place pages

---
+++ frontend/app/blog/[id]/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { useParams } from 'next/navigation';
+
+export default function Post() {
+    const { id } = useParams();
+
+    return <div>Post {id}</div>;
+}
--- frontend/app/blog/page.tsx
+++ frontend/app/blog/page.tsx
@@ -1,7 +1,9 @@
-import Blog from "@/components/blog/blog";
+import {PlaceClient} from "@/lib/requests/place.client";
+import BlogComponent from "@/components/blog/blog";
 
 export default async function BlogPage() {
+    const places = await PlaceClient.getRandomPlaces(5);
     return (
-        <Blog  />
+        <BlogComponent places={places} />
     );
 }
