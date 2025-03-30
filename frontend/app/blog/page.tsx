460e8be4fcc1801e27c650193f07af596c6b73a4
Alex Ivanov <oleksii120@gmail.com>
2025-03-30 20:21:43 +0300

    Enhance middleware to include blog routes and update role checks; refactor BlogPage for dynamic rendering

--- frontend/app/blog/page.tsx
+++ frontend/app/blog/page.tsx
@@ -1,9 +1,10 @@
-import {PlaceClient} from "@/lib/requests/place.client";
-import BlogComponent from "@/components/blog/blog";
+import { PlaceClient } from '@/lib/requests/place.client';
+import BlogComponent from '@/components/blog/blog';
+
+// Remove caching because of cookies
+export const dynamic = 'force-dynamic';
 
 export default async function BlogPage() {
-    const places = await PlaceClient.getRandomPlaces(5);
-    return (
-        <BlogComponent places={places} />
-    );
+  const places = await PlaceClient.getRandomPlaces(5);
+  return <BlogComponent places={places} />;
 }
