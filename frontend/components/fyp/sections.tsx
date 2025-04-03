5a4814d619d2624f748de584da24d63031b18664
daniel-shliakhetko <danielshliakhetko@gmail.com>
2025-04-03 21:56:35 +0300

    Add nearby section

--- frontend/components/fyp/sections.tsx
+++ frontend/components/fyp/sections.tsx
@@ -5,6 +5,8 @@ import { Place } from '@/lib/types/place';
 import { cn } from '@/lib/utils';
 import PlaceList from '@/components/cards/place-list/place-list';
 import styles from './fyp.module.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const sections = ['nearby', 'recommended', 'saved'] as const;
 
@@ -13,7 +15,37 @@ type SectionsProps = {
   activeSection: (typeof sections)[number];
 };
 
-export default function Sections({ places, activeSection }: SectionsProps) {
+export default function Sections({ places: serverPlaces, activeSection }: SectionsProps) {
+  const [places, setPlaces] = useState<Place[]>(serverPlaces);
+
+  useEffect(() => {
+    const fetchPlaces = async (latitude: number, longitude: number) => {
+      try {
+        const response = await axios.post(`/api/places/nearby`, { latitude, longitude });
+        console.log(response.data);
+        setPlaces(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    setPlaces(serverPlaces);
+    
+    if (activeSection !== 'nearby') return;
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          };
+          fetchPlaces(latitude, longitude);
+        },
+        (error) => console.error(error),
+      );
+    }
+  }, [activeSection, serverPlaces]);
+  
   const router = useRouter();
   return (
     <div className={styles.sections}>
