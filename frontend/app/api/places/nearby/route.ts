def3f4f6c321d6766011e2fe2fea1ef60ddc7495
daniel-shliakhetko <danielshliakhetko@gmail.com>
2025-04-03 21:56:35 +0300

    Add nearby section

---
+++ frontend/app/api/places/nearby/route.ts
@@ -0,0 +1,8 @@
+﻿import { NextRequest, NextResponse } from 'next/server';
+import { PlaceClient } from '@/lib/requests/place.client';
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const body = await request.json();
+  const chat = await PlaceClient.getNearbyPlaces(body.latitude, body.longitude, 2);
+  return NextResponse.json(chat);
+}
