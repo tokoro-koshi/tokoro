471c2517bf376283968245f288acc8f88b933b53
Ania Savchuk <Ania.Savchuk.ShI.2023@lpnu.ua>
2025-04-09 22:26:45 +0300

    Add About page component

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,5 @@
+import AboutComponent from "@/components/about/about";
+
+export default async function AboutPage() {
+    return <AboutComponent />;
+}
---
+++ frontend/lib/constants/about/team.ts
@@ -0,0 +1,27 @@
+export const team = [
+    {
+        name: 'Oleksii Ivanov',
+        position: 'Teach Lead',
+        image: '/placeholder.svg',
+    },
+    {
+        name: 'Ania Savchuk',
+        position: 'Frontend Developer',
+        image: '/placeholder.svg',
+    },
+    {
+        name: 'Yuliia Pedchenko',
+        position: 'Designer',
+        image: '/placeholder.svg',
+    },
+    {
+        name: 'Danylo Shliakhetko',
+        position: 'Frontend Developer',
+        image: '/placeholder.svg',
+    },
+    {
+        name: 'Oleh Popovych',
+        position: 'Backend Developer',
+        image: '/placeholder.svg',
+    },
+]
