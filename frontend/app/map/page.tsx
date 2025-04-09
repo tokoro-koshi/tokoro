4c4a84d37ccf0ac0be44585e74d1e922f43f41a7
Alex Ivanov <oleksii120@gmail.com>
2025-04-09 10:39:53 +0300

    Add user geolocation prompt on map page

--- frontend/app/map/page.tsx
+++ frontend/app/map/page.tsx
@@ -1,26 +1,10 @@
-﻿import GoogleMapComponent from '@/components/map/google-map';
-import { PlaceClient } from '@/lib/requests/place.client';
-import styles from './map.module.css';
+﻿import { PlaceClient } from '@/lib/requests/place.client';
+import MapWithGeoPrompt from '@/components/map/map-with-geoprompt';
 
 // Disable caching because of fetching that uses cookies
 export const dynamic = 'force-dynamic';
 
 export default async function MapPage() {
   const places = await PlaceClient.getAllPlaces(0, 1000);
-  // TODO: Change to user's location using geolocation API
-  const center = { lat: 49.841328512070056, lng: 24.02792769795437 };
-
-  return (
-    <div className={styles.pageContainer}>
-      <div className={styles.mapWrapper}>
-        <GoogleMapComponent
-          places={places}
-          center={center}
-          zoom={12}
-          doesShowDetails={true}
-          showsAll={true}
-        />
-      </div>
-    </div>
-  );
+  return <MapWithGeoPrompt places={places} />;
 }
