004a2f88f0dcd884f393bdefcca18d1e58be7b40
Alex Ivanov <oleksii120@gmail.com>
2025-04-10 18:09:41 +0300

    Force dynamic revalidation for pages using authed API requests

--- frontend/app/api/user/details/route.ts
+++ frontend/app/api/user/details/route.ts
@@ -1,6 +1,8 @@
 ﻿import { NextResponse } from 'next/server';
 import { UserClient } from '@/lib/requests/user.client';
 
+export const dynamic = 'force-dynamic'; // force revalidation because of auth
+
 export async function GET() {
   const user = await UserClient.getAuthenticatedUser();
   return NextResponse.json(user, { status: 200 });
