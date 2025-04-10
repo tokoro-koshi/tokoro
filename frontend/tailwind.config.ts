e344dc2b9f1793dd8ffbbf2b10d79595bc8d043e
Alex Ivanov <oleksii120@gmail.com>
2025-04-10 13:55:48 +0300

    Add contact us page

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './mdx-components.tsx',
   ],
   safelist: [
     {
